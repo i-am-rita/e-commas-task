d160140b51228410c24739a9bdf3aa669bf08ba6
User <user@Users-MacBook-Pro.local>
2022-01-14 00:00:38 +0100

    initial commit navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components.js/Navbar';
+import './index.css';
 
 function App() {
+  const name = () => {
+    console.log('Rita');
+  }
+  name();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <h1>Rita we need to get to work</h1> */}
+     <Navbar /> 
     </div>
   );
 }
