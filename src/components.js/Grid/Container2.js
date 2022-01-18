3a2e853ecfec4d508ab249dbfe5d14f26ed66ab4
unknown <ritauzoma07@gmail.com>
2022-01-18 17:08:27 +0100

    data

--- src/components.js/Grid/Container2.js
+++ src/components.js/Grid/Container2.js
@@ -1,14 +1,79 @@
 import React from 'react'
 import './Container2.css'
-// import sneaker1 from '../../images/sneaker1.png'
-// import brownbag from '../../images/brown-bag.png'
-// import strapbag from '../../images/strapbag.png'
+import sneaker1 from '../../images/sneaker1.png'
+import brownbag from '../../images/brown-bag.png'
+import strapbag from '../../images/strapbag.png'
 import addidas from '../../images/blueaddidas.png'
 import shoesole from '../../images/shoesole.png'
 import yellblack from '../../images/yellblack.png'
 import reactshoe from '../../images/reactshoe.png'
 
 export default function Grid() {
+    const data = [
+        {
+            img: sneaker1,
+            title: 'Nike Air Max 270 React',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+
+        {
+            img: sneaker1,
+            title: 'Nike Max Air 270 React',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+
+        {
+            img: sneaker1,
+            title: 'Nike 270 Air Max React',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+
+        {
+            img: sneaker1,
+            title: '270 Nike Air Max React',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+        
+        {
+            img: sneaker1,
+            title: 'React 270 Nike Air Max',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+
+        {
+            img: sneaker1,
+            title: 'Air React 270 Nike Max',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+
+        {
+            img: sneaker1,
+            title: 'Nike React 270  Air Max',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+
+        {
+            img: sneaker1,
+            title: 'Rita 270 Nike Air Max',
+            price1: '$299,43',
+            price2: '534,33',
+            price3: '24% Off'
+        },
+       ]
     return (
         <div>
         <h1>Best Seller</h1>
@@ -21,11 +86,7 @@ export default function Grid() {
             <li> <a href='#home'>Sunglasses</a></li> 
             </ul>
             </div>
-const data = {
- {
-     title = 'Nike Air Max 270 React';
- }   
-}
+
 
 
 
@@ -48,9 +109,9 @@ const data = {
         <div className='container'>
             {/* <div className='cards'> */}
                 <div className='items'>
-                <img src={sneaker1} alt='black bag' />
+                <img src={data.img} />
                 <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
+                    <h3>{data.title}</h3>
                     <p>
                     <i class="fas fa-star"></i>
                     <i class="fas fa-star"></i>
@@ -58,108 +119,9 @@ const data = {
                     <i class="fas fa-star"></i>
                     <i class="far fa-star"></i>
                     </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
+                    <p>{data.price1}<small>{data.price2}</small> <span>{data.price3}</span></p>
                 </div>
                 </div>
-
-                <div className='items'>
-                <img src={brownbag} alt='brown bag' />
-                <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
-                    <p><i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-                    </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
-                </div>
-                </div>
-
-                <div className='items'>
-                <img src={strapbag} alt='black bag' />
-                <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
-                    <p><i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-                    </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
-                </div>
-                </div>
-
-                <div className='items'>
-                <img src={addidas} alt='black bag' />
-                <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
-                    <p><i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-                    </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
-                </div>
-                </div>
-
-                <div className='items'>
-                <img src={shoesole} alt='black bag' />
-                <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
-                    <p><i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-                    </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
-                </div>
-                </div>
-
-                <div className='items'>
-                <img src={yellblack} alt='black bag' />
-                <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
-                    <p><i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-                    </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
-                </div>
-                </div>
-
-                <div className='items'>
-                <img src={reactshoe} alt='black bag' />
-                <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
-                    <p><i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-                    </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
-                </div>
-                </div>
-
-                <div className='items'>
-                <img src={addidas} alt='black bag' />
-                <div className='item'>
-                    <h3>Nike Air Max 270 React</h3>
-                    <p><i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-                    </p>
-                    <p>$299,43 <small>$534,33</small> <span>24% Off</span></p>
-                </div>
-                </div>
-            {/* </div> */}
         </div>
         </div>
     )
