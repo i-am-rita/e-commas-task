b2a47423f36d30d9340a933dc916c2e639ab82c5
Rita Uzoma <you@example.com>
2022-02-21 17:29:17 +0100

    Worked on Contact page using Formik and Yup for validation

--- src/components/Contactpage/Contactus.js
+++ src/components/Contactpage/Contactus.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {useFormik} from 'formik'
+import * as Yup from 'yup'
 // import bluebg from '../../images/bluebg.png'
 import callUs from '../../images/callUs.png'
 import './Contactus.css'
 export default function Contactpage() {
-  return <>
+  const formik = useFormik({
+    initialValues: {
+    firstName: "", 
+    lastName: "", 
+   textarea: "",
+  },
+  validationSchema: Yup.object({
+    firstName: Yup.string().min(15, "Must be 15 characters or less")
+    .required("Required"),
+    lastName: Yup.string().max(15, "Must be 15 characters or less")
+    .required("Required"),
+  }),
+  onSubmit: (values) => {
+    console.log(values)
+  }
+  });
+  console.log(formik.errors)
+  // console.log(formik.values)
+  // const formik = Formik
+  return (
+  <>
     <button className='buttondeal'>
       <p><span>Home</span> / Hot Deal </p>
     </button>
@@ -13,33 +35,56 @@ export default function Contactpage() {
       <img src={callUs} alt='' className='girlimg'/>
       </div>
       <div className='addres'>
-      {/* <h2>Temi show love by sending money</h2> */}
       <h1>get in <br/> touch</h1>
       <p>contact@e-comm.ng</p>
       <p>+234 4556 6665 34</p>
       <p>20 Prince Hakerem Lekki <br />Phase 1, Lagos</p>
       </div>
       </div>
+     
+      
+
       <div class="enter-details">
-  {/* <form action="action_page.php"> */}
-
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
-
-    <label for="lname">Email</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
-
-    <label for="subject">Message</label>
-    <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
-
-    <input type="submit" value="Submit"/>
-  {/* </form> */}
-  </div>
-</div>
-
-<div className='butto'>
+    <form onSubmit={formik.handleSubmit}>
+    {/* <div class="enter-details"> */}
+    <input 
+    id="fname"
+    type="text"  
+    name="firstname" 
+    placeholder="Your name.."
+    onChange={formik.handleChange}
+    values={formik.values.firstName}
+    />
+    {formik.errors.firstName ? <p>{formik.errors.firstName}</p> : null}
+    {/* </div> */}
+    {/* <div class="enter-details"> */}
+    <input 
+    id="lname"
+    type="text"  
+    name="lastname" 
+    placeholder="Your last name.."
+    onChange={formik.handleChange}
+    values={formik.values.lastName}
+    />
+     {formik.errors.lastName ? <p>{formik.errors.lastName}</p> : null}
+    {/* </div> */}
+    {/* <div class="enter-details"> */}
+    <textarea 
+    id="subject" 
+    name="subject" 
+    placeholder="Write something.." >
+    {/* onChange={formik.handleChange}
+    values={formik.values.textarea} */}
+    </textarea>
+    {/* </div> */}
+    <button type='submit'> Submit</button>
+    </form>
+    </div>
+    </div>
+    <div className='butto'>
         <input type='search' placeholder='Search query...'/>
         <button>Search</button>
         </div>
-  </>;
+ </>
+ )
 }
